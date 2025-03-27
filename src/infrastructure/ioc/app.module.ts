ef7d3a8be361139ebdadba0fcc7433a4fb397caf
Tomás Tapia <ttapia@kaisanetwork.net>
2025-03-27 17:21:56 -0300

    feat: add @nestjs/throttler for rate limiting and configure global throttling settings

--- src/infrastructure/ioc/app.module.ts
+++ src/infrastructure/ioc/app.module.ts
@@ -1,17 +1,23 @@
 import {
-  MiddlewareConsumer,
   Module,
+  MiddlewareConsumer,
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { getEnv } from '../env';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { InfrastructureModule } from './infrastructure.module';
 import { GCPLogger, RequestLogger } from '../logger';
 import { CompressionMiddleware } from '../adapters/inbound/http/middleware/compression.middleware';
 import { HelmetMiddleware } from '../adapters/inbound/http/middleware/helmet.middleware';
 import { AllExceptionsFilter } from '../adapters/inbound/http/filters/http-exception.filter';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { getEnv } from '../env';
 
+/**
+ * AppModule is the root module of the application.
+ * It imports necessary modules and sets up global configurations.
+ */
 @Module({
   imports: [
     InfrastructureModule,
@@ -20,19 +26,51 @@ import { AllExceptionsFilter } from '../adapters/inbound/http/filters/http-excep
       expandVariables: true,
       envFilePath: getEnv(),
     }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        throttlers: [
+          {
+            name: 'short',
+            ttl: configService.get<number>('THROTTLER_SHORT_TTL_SEC', 1) * 1000,
+            limit: configService.get<number>('THROTTLER_SHORT_LIMIT', 3),
+          },
+          {
+            name: 'medium',
+            ttl:
+              configService.get<number>('THROTTLER_MEDIUM_TTL_SEC', 10) * 1000,
+            limit: configService.get<number>('THROTTLER_MEDIUM_LIMIT', 5),
+          },
+          {
+            name: 'long',
+            ttl: configService.get<number>('THROTTLER_LONG_TTL_SEC', 60) * 1000,
+            limit: configService.get<number>('THROTTLER_LONG_LIMIT', 10),
+          },
+        ],
+      }),
+      inject: [ConfigService],
+    }),
   ],
-  providers: [GCPLogger, RequestLogger, AllExceptionsFilter],
+  providers: [
+    GCPLogger,
+    RequestLogger,
+    AllExceptionsFilter,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard, // Applies rate limiting globally
+    },
+  ],
   exports: [GCPLogger, RequestLogger, AllExceptionsFilter],
 })
 export class AppModule implements NestModule {
+  /**
+   * Configures middleware for the application.
+   * Applies RequestLogger, HelmetMiddleware, and CompressionMiddleware to all routes.
+   * @param consumer The middleware consumer.
+   */
   configure(consumer: MiddlewareConsumer) {
-    const middlewares = [
-      RequestLogger,
-      HelmetMiddleware,
-      CompressionMiddleware,
-    ];
     consumer
-      .apply(...middlewares)
+      .apply(RequestLogger, HelmetMiddleware, CompressionMiddleware)
       .forRoutes({ path: '*', method: RequestMethod.ALL });
   }
 }
